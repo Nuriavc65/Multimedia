3575ca85229f143ecfa796155205d9113b98e980
Nuria <nuriavc65@educastur.es>
2023-10-02 20:40:06 +0200

    moviles nuevo

--- contador/app/src/main/java/com/example/contador/MainActivity.java
+++ contador/app/src/main/java/com/example/contador/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     ImageView img;
     int valorClick = 1;
     int costeBillete = 100;
+    int monedas =9999;
     TextView nivel;
     ScaleAnimation fade_in = new ScaleAnimation(0.7f, 1.2f, 0.7f, 1.2f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
 
@@ -34,28 +35,22 @@ public class MainActivity extends AppCompatActivity {
         fade_in.setDuration(100);
     }
     public void sumar( View v){
-        int numero;
         //String fin;
-        numero = Integer.parseInt(contador.getText().toString());
-        numero = numero + valorClick;
+        monedas = monedas + valorClick;
         img.startAnimation(fade_in);
-        contador.setText(Integer.toString(numero)); //como texto es un String hay que pasar el numero a String
-        if(numero % 2 == 0){
+        if(monedas % 2 == 0){
             contador.setTextColor(Color.rgb(0,255,255));
         }else{
             contador.setTextColor(Color.rgb(0,255,0));
         }
-        if(numero == 39){
+        if(monedas == 39){
             contador.setTextColor(Color.rgb(255,0,0));
         }
-        //fin = Integer.toString(numero);
-        //contador.setText(fin);
-
-        //if(numero == 50){
-            //Button boton = (Button) findViewById(R.id.button);
-            //boton.setEnabled(false);
-            //contador.setText("parada");
-        //}
+        if(monedas >= 10000){
+            contador.setText(Double.toString(monedas/10000d).substring(0,3)+"M"); //1000d la de para obtener el numero decimal
+        }else {
+            contador.setText(Integer.toString(monedas)); //como texto es un String hay que pasar el numero a String
+        }
 
     }
     public void subidaDeNivel (View v) {
