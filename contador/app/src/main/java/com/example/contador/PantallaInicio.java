834d6db8ce447f9323ccfd3e7d82a4b95d46e48a
Nuria <nuriavc65@educastur.es>
2023-10-11 19:46:47 +0200

    nuevo soy tonta y puse mal el commit

--- contador/app/src/main/java/com/example/contador/PantallaInicio.java
+++ contador/app/src/main/java/com/example/contador/PantallaInicio.java
@@ -2,7 +2,9 @@ package com.example.contador;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class PantallaInicio extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class PantallaInicio extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_inicio);
+    }
+    public void pantallaJuego (View v) {
+        Intent abrir = new Intent(this,MainActivity.class);
+        startActivity(abrir);
+    }
+    public void salir(){
+        finish();
     }
 }
