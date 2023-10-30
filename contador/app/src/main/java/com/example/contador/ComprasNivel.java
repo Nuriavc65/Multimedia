0ae21cbb9b10fe8e2787b539988c38e204c40529
Nuria <nuriavc65@educastur.es>
2023-10-30 18:39:50 +0100

    soy gilipollas

--- contador/app/src/main/java/com/example/contador/ComprasNivel.java
+++ contador/app/src/main/java/com/example/contador/ComprasNivel.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -22,18 +23,22 @@ public class ComprasNivel extends AppCompatActivity {
     int valorAutoClick = 1;
     int costeBilleteAuto = 100;
     Bundle bundle;
+    TextView Contador;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compras_nivel);
+
         nivel = (TextView) findViewById(R.id.valorClicks);
         nivel2 = (TextView) findViewById(R.id.valorAutoClick);
-        botonAuto = (ImageView)findViewById(R.id.botonSubidaAuto);
-        setContentView(R.layout.activity_compras_nivel);
+        botonAuto = (ImageView) findViewById(R.id.botonSubidaAuto);
+
         sumarAuto();
         bundle = getIntent().getExtras();
         if (bundle != null) {
+            Log.d("nuria", "shop-nuria aprende a programar");
             monedas = new BigInteger(bundle.getString("monedas"));
             contador = bundle.getString("contador");
             valorClick = bundle.getInt("valorClick");
@@ -43,18 +48,20 @@ public class ComprasNivel extends AppCompatActivity {
 
 
     }
-    public void subidaDeNivel (View v) {
-        if(monedas.longValue() > costeBillete) { // si el valor del contador es mayor o igual al
+
+    public void subidaDeNivel(View v) {
+        if (monedas.longValue() > costeBillete) { // si el valor del contador es mayor o igual al
             monedas = monedas.subtract(new BigInteger(Integer.toString(costeBillete)));
             contador = monedas.toString();
             valorClick++;
             costeBillete = costeBillete + 10;
-            nivel.setText("valor: " +Integer.toString(costeBillete));
+            nivel.setText("valor: " + Integer.toString(costeBillete));
             monedas = monedas.add(BigInteger.valueOf(valorClick));
 
         }
     }
-    public void  sumarAuto() {
+
+    public void sumarAuto() {
         Thread hilo = new Thread(() -> {
             while (true) {
                 monedas = monedas.add(BigInteger.valueOf(valorAutoClick));
@@ -70,22 +77,25 @@ public class ComprasNivel extends AppCompatActivity {
         hilo.start();
 
     }
-    public void SubidaAutoClick(View v){
-        if(monedas.longValue()>costeBilleteAuto){
+
+    public void SubidaAutoClick(View v) {
+        if (monedas.longValue() > costeBilleteAuto) {
             monedas = monedas.subtract(new BigInteger(Integer.toString(costeBilleteAuto)));
             contador = monedas.toString();
             valorAutoClick++;
             costeBilleteAuto = costeBilleteAuto + 20;
-            nivel2.setText("valor: " +Integer.toString(costeBilleteAuto));
+            nivel2.setText("valor: " + Integer.toString(costeBilleteAuto));
         }
     }
-    public void volver (View v){
-        Intent n = new Intent(this,MainActivity.class);
+
+    public void volver(View v) {
+        Intent n = new Intent(this, MainActivity.class);
         n.putExtra("monedas", monedas.toString());
-        n.putExtra("contador",contador);
-        n.putExtra("valorClick",valorClick);
-        n.putExtra("mejoras",mejoras.toString());
-        n.putExtra("costeAuto",costeBilleteAuto);
+        n.putExtra("contador", contador);
+        n.putExtra("valorClick", valorClick);
+        n.putExtra("mejoras", mejoras.toString());
+        n.putExtra("costeAuto", costeBilleteAuto);
         startActivity(n);
+        finish();
     }
 }
--- contador/app/src/main/java/com/example/contador/MainActivity.java
+++ contador/app/src/main/java/com/example/contador/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.ScaleAnimation;
@@ -46,36 +47,39 @@ public class MainActivity extends AppCompatActivity {
         sumarAuto();
         bundle = getIntent().getExtras();
         if (bundle != null) {
-        monedas = new BigInteger(bundle.getString("monedas"));
-        contador.setText(bundle.getString("contador"));
-        valorClick = bundle.getInt("valorClick");
-        mejoras = new BigInteger(bundle.getString("mejoras"));
-        costeBilleteAuto = bundle.getInt("costeAuto");
+            Log.d("nuria", "main-aprende a programar");
+            monedas = new BigInteger(bundle.getString("monedas"));
+            contador.setText(bundle.getString("contador"));
+            valorClick = bundle.getInt("valorClick");
+            mejoras = new BigInteger(bundle.getString("mejoras"));
+            costeBilleteAuto = bundle.getInt("costeAuto");
 
         }
 
 
     }
-    public void sumar( View v){
+
+    public void sumar(View v) {
         monedas = monedas.add(BigInteger.valueOf(valorClick));
         img.startAnimation(fade_in);
-        if(monedas.remainder(BigInteger.valueOf(2)).equals(BigInteger.valueOf(0))){ //pares --> monedas%2==0
-            contador.setTextColor(Color.rgb(0,255,255));
-        }else{
-            contador.setTextColor(Color.rgb(0,255,0));
+        if (monedas.remainder(BigInteger.valueOf(2)).equals(BigInteger.valueOf(0))) { //pares --> monedas%2==0
+            contador.setTextColor(Color.rgb(0, 255, 255));
+        } else {
+            contador.setTextColor(Color.rgb(0, 255, 0));
         }
-        if(monedas.equals(BigInteger.valueOf(39))){         //cuando llegue a 39 color rojo
-            contador.setTextColor(Color.rgb(255,0,0));
+        if (monedas.equals(BigInteger.valueOf(39))) {         //cuando llegue a 39 color rojo
+            contador.setTextColor(Color.rgb(255, 0, 0));
         }
-        if(monedas.longValue()>10000){ //1000d d de double para obtener el numero decimal --> monedas >= 10000
-            decimal = BigDecimal.valueOf(monedas.longValue()/1000d);
-            contador.setText(decimal.toString().substring(0,1)+"M");
-        }else {
+        if (monedas.longValue() > 10000) { //1000d d de double para obtener el numero decimal --> monedas >= 10000
+            decimal = BigDecimal.valueOf(monedas.longValue() / 1000d);
+            contador.setText(decimal.toString().substring(0, 1) + "M");
+        } else {
             contador.setText(monedas.toString()); //como texto es un String hay que pasar el numero a String
         }
 
     }
-    public void  sumarAuto() {
+
+    public void sumarAuto() {
         Thread hilo = new Thread(() -> {
             while (true) {
                 monedas = monedas.add(BigInteger.valueOf(valorAutoClick));
@@ -92,18 +96,21 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void irInicio(View v){
-        Intent n = new Intent(this,PantallaInicio.class);
+    public void irInicio(View v) {
+        Intent n = new Intent(this, PantallaInicio.class);
         startActivity(n);
+        finish();
     }
-    public void comprasNivel (View v){
-        Intent n = new Intent(this,ComprasNivel.class);
-        n.putExtra("monedas",monedas.toString());
-        n.putExtra("contador",contador.toString());
-        n.putExtra("valorClick",valorClick);
-        n.putExtra("mejoras",mejoras.toString());
-        n.putExtra("costeAuto",costeBilleteAuto);
+
+    public void comprasNivel(View v) {
+        Intent n = new Intent(this, ComprasNivel.class);
+        n.putExtra("monedas", monedas.toString());
+        n.putExtra("contador", contador.toString());
+        n.putExtra("valorClick", valorClick);
+        n.putExtra("mejoras", mejoras.toString());
+        n.putExtra("costeAuto", costeBilleteAuto);
         startActivity(n);
+        finish();
 
     }
 
